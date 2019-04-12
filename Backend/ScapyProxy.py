7053831645ed781a39f5ce65738fb1d835f2a260
cwtovar <chriswtovar@gmail.com>
2019-04-12 15:06:21 -0600

    Skeleton Code of Scapy Proxy

--- Backend/ScapyProxy.py
+++ Backend/ScapyProxy.py
@@ -1,5 +1,18 @@
 #ScapyProxy
-#https://github.com/phaethon/kamene - reference
+#Reference- https://www2.mmu.ac.uk/media/mmuacuk/content/documents/school-of-computing-mathematics-and-digital-technology/blossom/PythonScriptingwithScapyLab.pdf 
 
 
 import scapy
+from scapy.all import *
+
+a=sniff(count=10)
+a.nsummary()
+
+send(IP(dst="184.0.0.0")/ICMP())
+sendp(Ether()/IP(dst="184.0.0.0",ttl=(1,4)),iface="eth1")
+
+ans,unans=sr(IP(dst="192.168.86.130",ttl=5)/ICMP())
+ans.nsummary()
+unans.nsummary()
+p=sr1(IP(dst="192.168.86.130")/ICMP()/"XXXXXX")
+p.show()
