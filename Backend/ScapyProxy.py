b840b791a7a7065e26f3c3399a62c7c68a51eabb
cwtovar <chriswtovar@gmail.com>
2019-04-17 16:05:11 -0600

    Update ScapyProxy.py

--- Backend/ScapyProxy.py
+++ Backend/ScapyProxy.py
@@ -1,18 +1,23 @@
 #ScapyProxy
 #Reference- https://www2.mmu.ac.uk/media/mmuacuk/content/documents/school-of-computing-mathematics-and-digital-technology/blossom/PythonScriptingwithScapyLab.pdf 
-
+#Reference - https://scapy.readthedocs.io/en/latest/usage.html#
 
 import scapy
 from scapy.all import *
 
-a=sniff(count=10)
-a.nsummary()
+def generaldemo():
+  a=sniff(count=10)
+  a.nsummary()
 
-send(IP(dst="184.0.0.0")/ICMP())
-sendp(Ether()/IP(dst="184.0.0.0",ttl=(1,4)),iface="eth1")
+  send(IP(dst="184.0.0.0")/ICMP())
+  sendp(Ether()/IP(dst="184.0.0.0",ttl=(1,4)),iface="eth1")
 
-ans,unans=sr(IP(dst="192.168.86.130",ttl=5)/ICMP())
-ans.nsummary()
-unans.nsummary()
-p=sr1(IP(dst="192.168.86.130")/ICMP()/"XXXXXX")
-p.show()
+  ans,unans=sr(IP(dst="192.168.86.130",ttl=5)/ICMP())
+  ans.nsummary()
+  unans.nsummary()
+  p=sr1(IP(dst="192.168.86.130")/ICMP()/"XXXXXX")
+  p.show()
+
+def gatherICMPPackets():
+  a=sniff(filter="icmp and host 66.35.250.151", count=2)
+  a.nsummary()
