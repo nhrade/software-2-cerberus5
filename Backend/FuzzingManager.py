a9512b56d28c152681ac72292e09a6e931a1345a
Noah Hradek <noahh717@gmail.com>
2019-04-24 13:04:02 -0600

    Added files

---
+++ Backend/FuzzingManager.py
@@ -0,0 +1,11 @@
+'''
+FuzzingManager.py
+'''
+
+class FuzzingManager:
+
+    def generateFuzzValue(self, field, vrange):
+        pass
+
+    def generateFuzzedPacket(self, packet, field, vrange, value):
+        pass
---
+++ Backend/Pypacker.py
@@ -0,0 +1,12 @@
+'''
+Fuzzer.py
+author: Noah Hradek
+'''
+
+import pypacker
+
+class Pypacker:
+
+
+    def createPacket(self):
+        pass
