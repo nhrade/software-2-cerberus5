4423d2694f793c52aad0e20fc42294bed2549bff
Noah Hradek <noahh717@gmail.com>
2019-05-09 00:35:12 -0600

    Changed files

--- Backend/FuzzingManager.py
+++ Backend/FuzzingManager.py
@@ -2,10 +2,21 @@
 FuzzingManager.py
 '''
 
+from pypacker.layer3.ip import IP
+from pypacker.layer3.icmp import ICMP
+
 class FuzzingManager:
 
     def generateFuzzValue(self, field, vrange):
         pass
 
     def generateFuzzedPacket(self, packet, field, vrange, value):
-        pass
+        return IP(src_s="127.0.0.1", dst_s="192.168.0.1", p=1) + \
+             ICMP(type=8) + \
+             ICMP.Echo(id=123, seq=1, body_bytes=b"foobar")
+
+
+
+fm = FuzzingManager()
+ip = fm.generateFuzzedPacket(None, None, None, None)
+print("%s" % ip)
