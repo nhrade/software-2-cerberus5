ce42ee2dcf85a071af1c7eda4981df3152fd0053
rgrenger <35977310+rgrenger@users.noreply.github.com>
2019-05-13 00:55:40 -0600

    Add files via upload
    This is the current state of the hook collection subsystem. Will continue to make further changes.

---
+++ Backend/HookCollection.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import hook;
+
+class HookCollection:
+	def __init__(self, name, sequenceNumber, status, description, associationCounter):
+		self.name = name
+        self.sequenceNumber = sequenceNumber
+		self.status = status
+		self.description = description
+		self.associationCounter = self.associationCounter
+	
+	def updateCollectionStatus(self):
+		if self.status = true
+			self.status = false
+		else:
+			self.status = true
+	
+	def editCollectionSequenceI(self, number):
+		self.sequenceNumber = number
+		
+	def updateHookStatus:
+	
+	def provideHookCollectionName:
+	
+	def searchForHook:
+	
+	def executeHooks:
