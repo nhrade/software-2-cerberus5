5764603bc6768ae0aca588f9805f2c2d9c1b4503
root <root@localhost.localdomain>
2019-05-16 04:53:31 -0400

    interceptor noe using nfqueue, internal queue now list

--- Backend/test/scapySniffer.py
+++ Backend/test/scapySniffer.py
@@ -2,49 +2,72 @@
 from scapy.all import *
 from threading import Thread, Event
 from time import sleep
+from netfilterqueue import NetfilterQueue
 import datetime
 
 
 class Sniffer(Thread):
-	def __init__(self, filter_=""):#interface="eth0",
+	def __init__(self, filter_="", file__ = "snifferPcap.pcap"):#interface="eth0",
 		super().__init__()
 
 		self.daemon = True
 		self.socket = None
+		self.file_ = file__
 		#self.interface = interface
 		self.filter_ = filter_
 		self.stop_sniffer = Event()
+		self.nfqueue = NetfilterQueue()
 
 	def run(self):
 		now = datetime.datetime.now()
-		file__ = str(now.strftime("%Y-%m-%d_%H.%M.%S"))+'.pcap'
-		open(file__, 'w').close
-		print(file__)
-		self.socket = conf.L2listen(
+		""""file__ = str(now.strftime("%Y-%m-%d_%H.%M.%S"))+'.pcap'
+
+		print(file__)"""
+		open(self.file_, 'w').close()
+		self.nfqueue.bind(1, self.modify_using_hooks)
+		self.nfqueue.run()
+		""""self.socket = conf.L2listen(
 		type=ETH_P_ALL,
 			#iface=self.interface,
 			filter=self.filter_
-		)
+		)"""
 
-		sniff(
-			opened_socket=self.socket,
-			prn=self.print_packet(file__) ,
-			stop_filter=self.should_stop_sniffer
-		)
+		#sniff(
+		#	opened_socket=self.socket,
+		#	prn=self.print_packet(file__) ,
+		#	stop_filter=self.should_stop_sniffer
+		#)
 
 	def join(self, timeout=None):
 		self.stop_sniffer.set()
+		self.nfqueue.unbind()
 		super().join(timeout)
 	
-	def should_stop_sniffer(self, packet):
-		return self.stop_sniffer.isSet()
+	def modify_using_hooks(self, packet):
+		pkt = IP(packet.get_payload())
+		packet.drop()
+		#modify packet
+		if not sniff(offline=pkt, filter=self.filter_):
+			print("packet Denied")
+			send(pkt)#############################
+			return
+		#send to enabled Hook Collections
+		wrpcap(self.file_, pkt, append = True)
+		print("packet Accepted")
+		send(pkt)#############################
+		return
+	
 
-	def print_packet(self, file__ : str):
+
+	""""def should_stop_sniffer(self, packet):
+		return self.stop_sniffer.isSet()"""
+
+	""""def print_packet(self, file__ : str):
 		print(file__ + 'file recieved\n')
 		def appendToPcap(capturedPacket):
 			print('Writing To PCAP' + file__)
 			capturedPacket.show()
 			#dissectPacket(capturedPacket)
 			wrpcap(file__, capturedPacket, append = True)
-		return appendToPcap
+		return appendToPcap"""
 
