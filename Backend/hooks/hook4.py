ce2ebdc866ede6555dcba2ef8d5d3bc5bc440ab0
root <root@localhost.localdomain>
2019-05-16 01:17:13 -0400

    changed hook4

--- Backend/hooks/hook4.py
+++ Backend/hooks/hook4.py
@@ -5,20 +5,20 @@ implements tcp handshake
 '''
 
 import random
+from scapy.all import *
 
 class Hook4:
 
     def __init__(self):
         self.hookName = 'hook4'
-        self.MAX_SEQ_NUM = 10000
-        self.seqNum = random.randint(self.MAX_SEQ_NUM)
 
     def run(self, packet):
         ip = IP(src=packet['IP'].dst, dst=packet['IP'].src)
         if 'TCP' in packet and packet['TCP'].flags=='S':
             ack = TCP(sport=packet['TCP'].dport,
                       dport=packet['TCP'].sport,
-                      flags='A', seq=self.seqNum,
+                      flags='A', seq=packet.seq + 1,
                       ack=packet.seq + 1)
             send(ip/ack)
-        return packet, False
+        return packet
+
