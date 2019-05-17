93075e49d60526bcd910f746feaccf67580299a4
rgrenger <35977310+rgrenger@users.noreply.github.com>
2019-05-16 22:02:38 -0600

    Update HookCollectionManager.py

--- Backend/HookCollectionManager.py
+++ Backend/HookCollectionManager.py
@@ -3,10 +3,12 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import QPushButton, QLabel, QLineEdit
 
-import hook
+import Hook
 import HookCollection
 
 class HookCollectionManager(QtWidgets.MainWindow, QPushButton):
+
+	hookCollectionList = []
 
 	def __init__(self):
 		super(MyWindow, self), __init__()
@@ -16,6 +18,15 @@ class HookCollectionManager(QtWidgets.MainWindow, QPushButton):
 		self.pushButton_3.clicked.connect(self.on_open3)
 		self.pushButton_2.clicked.connect(self.on_open1)
 		self.pushButton_4.clicked.connect(self.on_open4)
+		
+		firstHookCollection = hookCollectionList[0]
+		secondHookCollection = hookCollectionList[1]
+		
+		for h in firstHookCollection.listOfHooks:
+			self.comboBox_21.addItem(h.name + ", " + h.description + ", " + h.path)
+		
+		for h in secondHookCollection.listOfHooks:
+			self.comboBox_21.addItem(h.name + ", " + h.description + ", " + h.path)
 		
 		self.comboBox_21.clicked.connect(self.on_open21)
 		self.comboBox_21.clicked.connect(self.on_open22)
@@ -56,10 +67,13 @@ class OtherWindow1(QtWidgets.QtMainWindow, QPushButton):
 			self.myOtherWindow = HookCollectionDisabled()
 			self.myOtherWindow = show();
 			selection = False
-		global hookC = HookCollection(self.hookCollectionNameEdit.text(), self.executionSequenceEdit.text(), selection, self.descriptionEdit.text(), 0, ListOfHooks = [])
-		return hookC
+		hookC = HookCollection(self.hookCollectionNameEdit.text(), self.executionSequenceEdit.text(), selection, self.descriptionEdit.text(), 0, ListOfHooks = [])
+		HookCollectionManager.hookCollectionList.append(hookC)
 	
-	def executionStatus(self)
+	def executionStatus(self):
+		def __init__(self):
+			super(OtherWindow,self),__init__()
+			uic.loadUi("CreateEditHook.ui",self).show()
 
 	def homeMethod(self):
 		self.myOtherWindow = HookCollectionManager()
@@ -80,5 +94,23 @@ class HookCollectionDisabled(self):
 	def backMethod(self):
 		self.myOtherWindow = HookCollectionManager()
 		self.myOtherWindow = show();
+		
+class HookView(self):
+	def __init__(self):
+		super(OtherWindow,self),__init__()
+		uic.loadUi("HookView.ui",self).show()
+		
+		self.createHookButton.clicked.connect(self.createHook)
+
+class createHook(self):
+	def __init__(self):
+		super(OtherWindow,self),__init__()
+		uic.loadUi("CreateHook.ui",self).show()
+		
+		self.pushButton_2.clicked.connect(self.saveMethod)
+		self.pushButton_3.clicked.connect(self.on_openHome)
+
+	def clickMethod(self):
+		thisHook = Hook(self.lineEdit.text(), 0, self.lineEdit_2.text(), True, lineEdit_3.text())
 	
 
